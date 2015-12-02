659897bfb761413eb62873742e089c48415e7496
Arvind <arvind.rathod@students.stamford.edu>
2015-12-02 09:29:50 +0700

    Week 3 task 1

---
+++ Week3/js/window-example.js
@@ -0,0 +1,28 @@
+var myWindow = {
+	screenX: window.screenX,
+	screenY: window.screenY,
+    screenWidth: window.screen.width,
+	screenHeight: window.screen.height,
+	
+	location: window.location.pathname,
+
+	render: function() {
+		var ele = document.getElementById("location");
+		ele.value = this.location;
+
+		ele = document.getElementById("sx");
+		ele.value = this.screenX;
+	
+	ele = document.getElementById("sy");
+	ele.value = this.screenY;
+
+	ele = document.getElementById("sxW");
+		ele.value = this.screenWidth;
+	
+	ele = document.getElementById("syH");
+	ele.value = this.screenHeight;	
+	}
+};
+
+
+myWindow.render();
