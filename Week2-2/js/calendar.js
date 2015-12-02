629d9cf052eb745d1d8440e0558accc7fd1d80ff
Arvind <arvind.rathod@students.stamford.edu>
2015-12-02 21:04:14 +0700

    Stamford hotel assignment

--- Week2-2/js/calendar.js
+++ Week2-2/js/calendar.js
@@ -1,21 +1,20 @@
 // display the calendar here
 var hotelTable = document.getElementById("hotelCalendarTable");
 var defaultData = hotelTable.innerHTML;
-var days = 1;
+var days = 2;
 var stringHTML = "";
-stringHTML = "<tr><td>1</td><td>2</td></tr>";
-// stringHTML = "<tr>";
-// hotelTable.innerHTML += stringHTML;
-// while(days<=31){
-// if(days=8 || days = 15 || days = 22 || days = 29){
-//  stringHTML += "</tr><br><tr><td>"+days+"</td>";
-//  hotelTable.innerHTML += stringHTML;
-// } 
-// else{
-// 	stringHTML += "<td>"+days+"</td>";
-// 	hotelTable.innerHTML += stringHTML;
-// }
-// days++;
-// }
-// stringHTML += "</tr>";
+//stringHTML = "<tr><td>1</td><td>2</td></tr>";
+stringHTML += "<tr><td>1</td>";
+//hotelTable.innerHTML += stringHTML;
+while(days<32){
+	if(days%7==1){
+		stringHTML += "</tr><br><tr><td>"+ days +"</td>";
+	} else { 
+		stringHTML += "<td>"+days+"</td>";
+	}
+
+	
+days++;
+}
+stringHTML += "</tr>";
 hotelTable.innerHTML += stringHTML;
