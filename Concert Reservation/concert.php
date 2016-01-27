6cf230501f3930ab7313a174bd14d782f714a169
Arvind <arvind.rathod@students.stamford.edu>
2016-01-27 23:33:47 +0700

    Concert Reservation Website

---
+++ Concert Reservation/concert.php
@@ -0,0 +1,44 @@
+<?php
+//echo "Hello World..";
+echo "<!DOCTYPE HTML>
+<html>
+<head>
+
+<title> Concert Reservation Website</title>
+<link href='css/style.css' rel='stylesheet' type='text/css'/>
+<style>
+body {
+background-image: url('images/back2.jpg');
+}
+</style>
+</head>
+<body background-image: url('images/back1.jpg')>
+<h1> Welcome to Concert Reservation</h1>
+ <div id='click'>";
+
+$seats = ['A-1', 'A-2', 'A-3', 'A-4', 'A-5', 
+      'B-1', 'B-2', 'B-3', 'B-4', 'B-5',
+      'C-1', 'C-2', 'C-3', 'C-4', 'C-5'];
+
+$i = 1;
+
+echo "<form action='concert_Result.php'
+   method='POST'>";
+
+    foreach ($seats as $seat) {
+               
+        echo "<input type='checkbox' name='seats[]' value='" .$seat. "'> </input>";
+        echo ($seat);
+    	 if($i%5==0) {
+    		echo ("<br>");
+    	
+    }
+    ++$i;
+}
+
+echo "<input type ='submit' value='Buy Now' /></form>";
+echo "</div>
+</body>
+</html>";
+
+?>
---
+++ Concert Reservation/concert_result.php
@@ -0,0 +1,58 @@
+<?php
+  //print_r($_POST["seats"]) ;
+echo "<!DOCTYPE HTML>
+<html>
+<head>
+<title> Concert Reservation Website</title>
+<link href='css/style.css' rel='stylesheet' type='text/css'/>
+<style>
+body {
+background-image: url('images/back2.jpg');
+}
+</style>
+</head>
+<body>
+<h1> Welocme to Concert Reservation</h1>
+ <div id='display'>";
+ 
+if( empty( $_POST["seats"])){
+	      echo " Sorry !!!  You have not booked any ticket seat(s)<br>";
+		   echo "Please select the ticket between 1 to  4 ";
+	} elseif( count( $_POST["seats"]) > 4 ){
+		 echo "You are trying buy " . count( $_POST["seats"]). " seat(s)<br>";
+		   echo "Sorry Your booked no. of booking can not be possible . Please select the ticket between 1 to  4 ";
+	}
+	 else{
+        $selectedSeats = $_POST["seats"];
+       echo "You have bought " . count($selectedSeats). " seat(s)<br>";
+       $totalPrice = 0;
+
+foreach ($selectedSeats as $selectedSeat) {
+	  $price = 0;
+    	echo $selectedSeat;
+
+
+   if ($selectedSeat[0] == "A") {
+         $price = 3000;
+
+}   elseif ($selectedSeat[0] == "B") {
+		$price = 2000;
+	}
+	else {
+		$price = 1000;
+	}
+	if($selectedSeat == "A-4"){
+	    $price = 3000 * .50;
+	    echo ":  ".$price ."<br>";
+	    echo "Congratulation!! You have the lucky draw ticket. This A-4 seat price is 50% off!<br>";
+	} else{
+        echo ":  ".$price ."<br>";
+		}
+       $totalPrice += $price;
+}
+    echo "Tatal Price is: " . $totalPrice. " THB";
+}
+echo "</div>
+</body>
+</html>";
+?>
